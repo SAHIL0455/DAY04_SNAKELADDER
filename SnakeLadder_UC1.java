1aec1b935bad5d3290082f8d5ff6cba8236562ad
SAHIL0455 <sahilpawar1004@gmail.com>
2022-05-25 01:07:21 +0530

    Starting position of player

---
+++ SnakeLadder_UC1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SnakeLadder_UC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder program");
+        int position = 0;
+        System.out.println("Your Starting Position is "+position);
+    }
+}
